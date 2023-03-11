a66fc4519cbd072e6f1eef534674cffd0314083a
BoxXt <127529953+BoxXt@users.noreply.github.com>
2023-03-11 14:19:35 +0800

    Add files via upload

--- installReality.sh
+++ installReality.sh
@@ -29,6 +29,8 @@ echo "安装singbox"
 
 echo "拉取初始singbox配置文件"
 curl -o config.json https://raw.githubusercontent.com/BoxXt/installReality/main/config.json && mv config.json /usr/local/etc/sing-box/config.json
+curl -o meta.yaml https://raw.githubusercontent.com/BoxXt/installReality/main/meta.yaml && mv meta.yaml /usr/local/etc/sing-box/meta.yaml
+
 
 echo "启动singbox用于安装reality"
 echo "生成密钥对,生成之后请保存好你的密钥对"
@@ -38,6 +40,8 @@ echo -n "是否已经保存好你的密钥对?[y/n]"
 read name
 echo -n "请输入你刚刚保存的密钥对的私钥（private_key）:"
 read pkey
+echo -n "请输入你刚刚保存的密钥对的公钥（pubilc_key）:"
+read pukey
 sed 's/\bpkey\b/$pkey/g' /usr/local/etc/sing-box/config.json
 
 echo "生成uuid"
@@ -46,26 +50,34 @@ echo $uuid
 echo -n "是否已经保存好你的uuid?[y/n]"
 sed 's/\bpuuid\b/$uuid/g' /usr/local/etc/sing-box/config.json
 
-echo "随机生成short_id"
 echo "安装openssl"
 sudo apt-get update && sudo apt-get install openssl
 shortid=`openssl rand -hex 4`
+echo "随机生成short_id"
 echo $shortid
-echo -n "是否已经保存好你的short_id?[y/n]"
 sed 's/\bpshortid\b/$shortid/g' /usr/local/etc/sing-box/config.json
 
 echo "完成配置，启动singbox"
 
-
 systemctl start sing-box
 
-echo "完成搭建"
 echo "以下是支持reality的meta客户端所需要的配置信息："
 echo "servername: www.microsoft.com"
 echo "flow: xtls-rprx-vision"
 echo "uuid: $uuid"
 echo "key: $result"
 echo "short_id: $shortid"
+
+echo -n "输入此vps的ip用于提供meta客户端所需要的配置文件示例："
+read vpsip
+sed 's/\vpsip\b/$vpsip/g' /usr/local/etc/sing-box/meta.yaml
+sed 's/\bpukey\b/$pukey/g' /usr/local/etc/sing-box/meta.yaml
+sed 's/\pshortid\b/$shortid/g' /usr/local/etc/sing-box/meta.yaml
+sed 's/\puuid\b/$uuid/g' /usr/local/etc/sing-box/meta.yaml
+echo "以下是你的meta客户端所需要的可用示例配置文件："
+cat /usr/local/etc/sing-box/meta.yaml
+echo "完成搭建"
+
 
 
 
